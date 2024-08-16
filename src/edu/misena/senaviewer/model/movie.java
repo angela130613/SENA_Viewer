5ed10bb5222729e728a7919dfb753302fcdce7a8
Angela Yepes <angelyep76@gmail.com>
2024-08-16 15:21:31 -0500

    public movie

--- src/edu/misena/senaviewer/model/movie.java
+++ src/edu/misena/senaviewer/model/movie.java
@@ -1,5 +1,21 @@
 package edu.misena.senaviewer.model;
 
-public class movie {
 
-}
+    public class movie {
+        int id;
+        String title;
+        String genre;
+        String creator;
+        int duracion;
+        short year;
+        boolean viewed;
+        int timeviwed;
+
+        public Movie(String title, String genre, String creator, int  duracion, short year){
+            this.title = title;
+            this.genre = genre;
+            this.creator = creator;
+            this.duracion = duracion;
+            this.year = year;
+        }
+    }
