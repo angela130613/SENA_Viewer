7f3a7ea42bb9c0e469b7dd8571b52ed88ca8298b
Angela Yepes <angelyep76@gmail.com>
2024-08-17 09:06:31 -0500

    public class chapter

--- src/edu/misena/senaviewer/model/chapter.java
+++ src/edu/misena/senaviewer/model/chapter.java
@@ -1,4 +1,13 @@
 package edu.misena.senaviewer.model;
 
 public class chapter {
+    int id;
+    String tittle;
+    int duration;
+    int year;
+    boolean viewed;
+    int timeViwed;
+    int sessionNumber;
+
+
 }
