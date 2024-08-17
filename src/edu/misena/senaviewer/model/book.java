6b8c5b8079f3130430a65ecfc9d65ac3f589ef8e
Angela Yepes <angelyep76@gmail.com>
2024-08-17 09:31:50 -0500

    public class book

--- src/edu/misena/senaviewer/model/book.java
+++ src/edu/misena/senaviewer/model/book.java
@@ -3,4 +3,22 @@ package edu.misena.senaviewer.model;
 import java.util.Date;
 import java.util.List;
 
+public class book {
+    int id;
+    String  title;
+    Date editorialDate;
+    String editorial;
+    List<String> authors;
+    String isbn;
+    boolean readed;
+    int timeReaded;
+
+    public  book(String title, Date editorialDate, String editorial, String isbn){
+        this.title = title;
+       this.editorialDate = editorialDate;
+        this.editorial = editorial;
+        this.isbn = isbn;
+    }
+}
+
 
