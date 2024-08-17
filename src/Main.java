bc6bba77d55df2f38ebeab51aef55d2f2b8cfd37
Angela Yepes <angelyep76@gmail.com>
2024-08-17 11:28:36 -0500

    public class Main and void

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import edu.misena.senaviewer.model.movie;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+
 public class Main {
+    static List<movie> movies = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+        int option = 1;
+
+        while (option != 0){
+            showMainMenu();
+        }
 
     }
 }
