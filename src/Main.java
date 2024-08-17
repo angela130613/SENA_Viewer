139e34bc5819b4afaf5883eec44e9b0bf86d510e
Angela Yepes <angelyep76@gmail.com>
2024-08-17 14:46:35 -0500

    static void and static private void

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,18 @@ import java.util.Scanner;
 
 
 public class Main {
-    static List<movie> movies = new ArrayList<>();
+    static List<Object> movies = new ArrayList<Object>();
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         int option = 1;
 
-        while (option != 0){
+        while (option != 0) {
             showMainMenu();
             option = scanner.nextInt();
 
-            switch (option){
+            switch (option) {
                 case 1:
                     manageMovies();
                     break;
@@ -45,7 +45,7 @@ public class Main {
 
     }
 
-    static void showMainMenu(){
+    static void showMainMenu() {
         System.out.println("Main Menu:");
         System.out.println("1. Movies");
         System.out.println("2. Series");
@@ -57,11 +57,55 @@ public class Main {
         System.out.print("Select an option: ");
     }
 
-    static void manageMovies(){
+    static void manageMovies() {
         System.out.println("Movie");
-        System.out.println("1. Agregar Pelicula");
+        System.out.println("1. Agregar pelucula");
         System.out.println("2. Ver peliculas");
-        System.out.println("Selecciona una opcion");
+        System.out.println("Seleciona una opcion");
         int option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                addMovie();
+                break;
+            case 2:
+                viewMovies();
+                break;
+            default:
+
+                System.out.println("Invalid option.");
+                break;
+        }
     }
+
+    private static void viewMovies() {
+    }
+
+    private static void addMovie() {
+        System.out.print("Enter movie title: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Enter movie genre: ");
+        String genre = scanner.nextLine();
+
+        System.out.print("Enter movie creator: ");
+        String creator = scanner.nextLine();
+
+        System.out.print("Enter movie duration (in minutes): ");
+        int duration = scanner.nextInt();
+
+        System.out.print("Enter movie year: ");
+        int year = scanner.nextInt();
+        scanner.nextLine(); // Consumir la nueva línea
+
+        // Crear una nueva instancia de Movie
+        Object movie = new movie(title, genre, creator, duration, year);
+
+        // Agregar la película a la lista
+        movies.add(movie);
+        System.out.println("Movie added successfully!");
 }
+}
+
+
+
