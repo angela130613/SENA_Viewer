f74aadaeff3edc5c746e96c5e5c597ab2c02e0c6
Angela Yepes <angelyep76@gmail.com>
2024-08-17 08:58:28 -0500

    public class serie

--- src/edu/misena/senaviewer/model/serie.java
+++ src/edu/misena/senaviewer/model/serie.java
@@ -1,4 +1,21 @@
 package edu.misena.senaviewer.model;
 
 public class serie {
+        int id;
+        String title;
+        String genre;
+        String creator;
+        int duration;
+        int year;
+        boolean viewed;
+        int timeViewed;
+        int sessionQuantity;
+
+        public serie(String title, String genre, int duration) {
+            this.title = title;
+            this.genre = genre;
+            this.duration = duration;
+        }
+
 }
+
