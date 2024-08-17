76f0ea560e57496226aa4542554060bc6dc580b7
Angela Yepes <angelyep76@gmail.com>
2024-08-17 09:43:18 -0500

    List<String> and import java.util.List

--- src/edu/misena/senaviewer/model/magazine.java
+++ src/edu/misena/senaviewer/model/magazine.java
@@ -1,11 +1,13 @@
 package edu.misena.senaviewer.model;
 
 import java.util.Date;
+import java.util.List;
 
 public class magazine {
     int id;
     String title;
     Date editorialDate;
     String editorial;
+    List<String> authors;
 
 }
