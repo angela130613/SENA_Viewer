c0e10af5fa8f2ea7a751d22fc170059165613dc0
Angela Yepes <angelyep76@gmail.com>
2024-08-17 23:31:49 -0500

    serie public void, setters and getters

--- src/edu/misena/senaviewer/model/serie.java
+++ src/edu/misena/senaviewer/model/serie.java
@@ -17,5 +17,78 @@ public class serie {
             this.duration = duration;
         }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    public void setGenre(String genre){
+            this.genre = genre;
+    }
+
+    public String getCreator(){
+            return creator;
+    }
+
+    public void setCreator(String creator){
+            this.creator = creator;
+    }
+
+    public int getDuration(){
+            return duration;
+    }
+
+    public void setDuration(int duration){
+            this.duration = duration;
+    }
+
+    public int getyear(){
+            return year;
+    }
+
+    public void  setYear(int year){
+            this.year = year;
+    }
+
+    public boolean getviewed(){
+            return viewed;
+    }
+
+    public void setViewed(boolean viewed){
+            this.viewed = viewed;
+    }
+
+    public int getTimeViewed(){
+            return timeViewed;
+    }
+
+    public void setTimeViewed(int timeViewed){
+            this.timeViewed = timeViewed;
+    }
+
+    public  int getSessionQuantity(){
+            return sessionQuantity;
+    }
+
+    public void setSessionQuantity(int sessionQuantity){
+        this.sessionQuantity = sessionQuantity;
+    }
+
+
 }
 
