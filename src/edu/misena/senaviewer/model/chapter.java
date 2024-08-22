d8342d158f43a5264fb86318ec641bf80edf48c2
Angela Yepes <angelyep76@gmail.com>
2024-08-22 08:11:38 -0500

    chapter get and set

--- src/edu/misena/senaviewer/model/chapter.java
+++ src/edu/misena/senaviewer/model/chapter.java
@@ -2,7 +2,7 @@ package edu.misena.senaviewer.model;
 
 public class chapter {
     int id;
-    String tittle;
+    String title;
     int duration;
     int year;
     boolean viewed;
@@ -10,10 +10,35 @@ public class chapter {
     int sessionNumber;
 
     public  chapter(String tittle, int duration, int year){
-        this.tittle = tittle;
+        this.title = tittle;
         this.duration = duration;
         this.year = year;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public int getDuration(){
+        return duration;
+    }
+
+    public void setDuration(int duration){
+        this.duration = duration;
+}
+
 
 
 }
